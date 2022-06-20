8a251ebb6a0f6de68e5e6e43ccbc0509fb2af15a
davidTruta <105377438+davidTruta@users.noreply.github.com>
2022-06-20 12:53:32 +0300

    repo test template

--- my_albums_app/test/repo/profile_repo_test.dart
+++ my_albums_app/test/repo/profile_repo_test.dart
@@ -1,1 +1,40 @@
-//TODO
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_albums_app/model/address.dart';
+import 'package:my_albums_app/model/profile.dart';
+import 'package:my_albums_app/repo/profile_repo.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  group("profile_repo - ", () {
+    ProfileRepo? profileRepo;
+    Profile? mockProfile;
+
+    //This will run before every test
+    setUp(() {
+      profileRepo = ProfileRepo(SharedPreferences.getInstance());
+      SharedPreferences.setMockInitialValues({});
+      mockProfile = Profile(
+        firstName: 'first',
+        lastName: 'last',
+        email: 'email@gmail.com',
+        phone: '0712345678',
+        address: Address(
+          street: 'str',
+          city: 'cit',
+          country: 'cou',
+          zipCode: '123'
+        )
+      );
+    });
+
+    test('getProfile() test case: should fetch profile from local storage', () async {
+
+    });
+
+    test('setAlbums() test case: should save profile to local storage', () async {
+
+    });
+
+  });
+}
